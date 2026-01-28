6fac693b7f0d5c36df016cda9e0cbb04ad3b508e
Sanket1516 <sanketmali0909@gmail.com>
2026-01-28 22:07:07 +0530

    new array question

--- Character Array.cpp
+++ Character Array.cpp
@@ -729,3 +729,25 @@ public:
         return ans;
     }
 };
+
+//reverse the vovels in the string LeetCode 345 (Easy)
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+               c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    }
+
+    string reverseVowels(string s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !isVowel(s[i])) i++;
+            while (i < j && !isVowel(s[j])) j--;
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
