f15073d45924f3c35564b69d70e7f2343c332134
Sanket1516 <sanketmali0909@gmail.com>
2026-07-08 13:07:48 +0530

    Add initial implementation of Trees data structure

---
+++ Trees.cpp
@@ -0,0 +1,360 @@
+//Implimentation
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* Left;
+    Node* Right;
+
+    Node(int data){
+        this->data = data;
+        Left = NULL;
+        Right = NULL;
+    }
+
+    Node* createTree(){
+        int data;
+        cout<<"Enter the data for the node "<<endl;
+        cin>>data;
+
+        if(data == -1){
+            return NULL;
+        }
+
+        Node* root = new Node(data);
+
+        cout<<"Enter the data for the left node of "<<data<<endl;
+        root->Left = createTree();
+        cout<<"Enter the data for the right node of "<<data<<endl;
+        root->Right = createTree();
+
+        return root;
+
+    }
+
+    //Traversal of the tree
+    void postOrder(Node* root){
+        if(root == NULL){
+            return;
+        }
+
+        //root
+        cout<<root->data<<" ";
+
+        //left
+        postOrder(root->Left);
+
+        //right
+        postOrder(root->Right);
+    }
+
+    void preOrder(Node* root){
+        if(root == NULL){
+            return;
+        }
+
+        //root
+        cout<<root->data<<" ";
+
+        //left
+        preOrder(root->Left);
+
+        //right
+        preOrder(root->Right);
+    }
+
+    void inOrder(Node* root){
+        if(root == NULL){
+            return;
+        }
+
+        //left
+        inOrder(root->Left);
+
+        //root
+        cout<<root->data<<" ";
+
+        //right
+        inOrder(root->Right);
+    }
+
+    void levelOrder(Node* root){
+        queue<Node*>q;
+
+        q.push(root);
+
+        while(!q.empty()){
+            Node* temp = q.front();
+            cout<<temp->data;
+            q.pop();
+
+            if(temp->left!=NULL){
+                q.push(temp->left);
+            }
+
+            if(temp->right!=NULL){
+                q.push(temp->right);
+            }
+        }
+    }
+
+    //Levelorder by newLine each level
+    void levelOrderNewline(Node* root){
+        queue<Node*>q;
+
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            Node* temp = q.front();
+
+            if(temp == NULL){
+                cout<<endl;
+                q.pop();
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }
+
+            else{
+            cout<<temp->data;
+            q.pop();
+
+            if(temp->left!=NULL){
+                q.push(temp->left);
+            }
+
+            if(temp->right!=NULL){
+                q.push(temp->right);
+            }
+        }
+    }
+    }
+
+};
+
+int main(){
+    //created a dummy object to call the function as it is not static
+    Node* temp = new Node(0);
+    Node* root = temp->createTree();
+    cout<<"The tree is created successfully "<<root->data<<endl;
+    temp->inOrder();
+    cout<<endl;
+    temp->preOrder();
+    cout<<endl;
+    temp->postOrder();
+    cout<<endl;
+    temp->levelOrder();
+    cout<<endl;
+
+    return 0;
+}
+
+//Creation of generic tree 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    vector<Node*> child;
+
+    Node(int data) {
+        this->data = data;
+    }
+
+    Node* createTree() {
+        int data;
+        cout << "Enter node data (-1 for NULL): ";
+        cin >> data;
+
+        if (data == -1)
+            return NULL;
+
+        Node* root = new Node(data);
+
+        int n;
+        cout << "Enter number of children of " << data << ": ";
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cout << "Enter child " << i + 1 << " of " << data << endl;
+            Node* newChild = createTree();
+            root->child.push_back(newChild);
+        }
+
+        return root;
+    }
+};
+
+int main() {
+    Node temp(0);                 // Dummy object
+    Node* root = temp.createTree();
+
+    cout << "\nTree created successfully." << endl;
+    cout << "Root node = " << root->data << endl;
+
+    return 0;
+}
+
+
+//Heigh of a tree leetcode 104
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int height = 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+
+            if(temp == NULL){
+                height++;
+                q.pop();
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }
+
+            else{
+                if(temp->left != NULL){
+                q.push(temp->left);
+                }
+
+                if(temp->right != NULL){
+                q.push(temp->right);
+                }
+                q.pop();
+            }
+        }
+        return height;
+    }
+};
+
+//Balanced Binary Tree Leetcode 110
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     int height(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int height = 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+
+            if(temp == NULL){
+                height++;
+                q.pop();
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }
+
+            else{
+                if(temp->left != NULL){
+                q.push(temp->left);
+                }
+
+                if(temp->right != NULL){
+                q.push(temp->right);
+                }
+                q.pop();
+            }
+        }
+        return height;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL){
+            return true;
+        }
+
+        bool leftisbalanced = isBalanced(root->left);
+        bool rightisbalanced = isBalanced(root->right);
+
+        int leftheight = height(root->left);
+        int rightheight = height(root->right);
+        int absdiff = abs(leftheight-rightheight);
+        bool isbalanced = (absdiff<=1);
+
+        if(isbalanced && leftisbalanced && rightisbalanced){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+//Lowest Common Ansector
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL){
+            return NULL;
+        }
+        if(root->val == p->val){
+            return p;
+        }
+        if(root->val == q->val){
+            return q;
+        }
+
+        TreeNode* leftans = lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightans = lowestCommonAncestor(root->right,p,q);
+
+        if(leftans == NULL && rightans == NULL){
+            return NULL;
+        }
+        if(leftans == NULL && rightans !=NULL){
+            return rightans;
+        }
+        if(leftans != NULL && rightans == NULL){
+            return leftans;
+        }
+        else{
+            return root;
+        }
+    }
+};
